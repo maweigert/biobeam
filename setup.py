7a06368331e61de1256d24b0827b92927dd90d2d
Martin Weigert <mweigert@mpi-cbg.de>
2016-08-09 14:41:01 +0200

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='biobeam',
-    version='0.1.0.2',
+    version='0.1.1',
     description='beam propagation',
     url='https://maweigert.github.io/biobeam',
     author='Martin Weigert',
@@ -14,7 +14,7 @@ setup(name='biobeam',
         'numpy',
         'scipy',
         "pyopencl>=2016.1",
-        'gputools',
+        'gputools>=0.1.3',
     ],
 
     package_data={"biobeam":['core/kernels/*.cl'
